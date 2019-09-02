bd6d0e8b4dedfea314e6442d46688c7989c68905
Nikolay Kisselev <russland.div@gmail.com>
2019-09-02 12:04:27 +0600

    Add project files.

---
+++ EurasianTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EurasianTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
